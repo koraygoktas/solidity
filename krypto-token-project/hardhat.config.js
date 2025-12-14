2de98695fd57e1921e5e46c8127e96d8fc375d44
koraygoktas <krygoktas123@gmail.com>
2025-12-14 22:04:26 +0300

    krypto

--- krypto-token-project/hardhat.config.js
+++ krypto-token-project/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 11155111
+    }
+  }
 };
