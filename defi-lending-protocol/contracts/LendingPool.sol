ff22ad8fc50e442d7afd193aa60cf1d206f47f2b
koraygoktas <krygoktas123@gmail.com>
2026-01-09 05:27:28 +0300

    comment lines

--- defi-lending-protocol/contracts/LendingPool.sol
+++ defi-lending-protocol/contracts/LendingPool.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; //belirli fonkları içermesini sağlıyor
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol"; //bazıları false olsa dahi revert olmaz onun için
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol"; //aynı anda işleme girilmesini engelliyor
 import "./InterestRateModel.sol";
 import "./PriceOracle.sol";
 
